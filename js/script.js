37557e5f4114267c6aad0460cd3694dab48f07b2
Patrick Tavares dos Santos <ptsantosrj@gmail.com>
2024-09-03 19:46:08 -0300

    Iniciando a construção do java script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
-const apiKey = 'ea7427211a3e48c89f4215950240309';
+const Key = 'ea7427211a3e48c89f4215950240309';
+//Carregar a previsao atual para determinada cidade: http://api.weatherapi.com/v1/current.json?key=<YOUR_API_KEY>&q=<city>
+//http://api.weatherapi.com/v1/current.json?key=ea7427211a3e48c89f4215950240309&q=London
