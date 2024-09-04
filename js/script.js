6e82f983fc08771f7b377f53f94453d3c3ac38d6
Patrick Tavares dos Santos <ptsantosrj@gmail.com>
2024-09-04 06:37:32 -0300

    Ajuste layout

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const key = 'ea7427211a3e48c89f4215950240309';
 //Carregar a previsao atual para determinada cidade: http://api.weatherapi.com/v1/current.json?key=<YOUR_API_KEY>&q=<city>
 //http://api.weatherapi.com/v1/current.json?key=ea7427211a3e48c89f4215950240309&q=London
-// Substitua 'YOUR_API_KEY' pela sua chave da WeatherAPI.com
 
 document.getElementById('botao-busca').addEventListener('click', () => {
     const cidade = document.getElementById('busca-cidade').value;
